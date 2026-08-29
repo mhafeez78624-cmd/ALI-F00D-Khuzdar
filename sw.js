9d7790472d6312c7fe3a6d280d1a1101b6e42fee
mhafeez78624-cmd <mhafeez78624@gmail.com>
2026-08-30 01:23:46 +0500

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
 const CACHE_NAME = "ali-food-khuzdar-v1";
 const ASSETS = [
-  "./",
+  "/",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./icon-512-maskable.png"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
@@ -16,27 +13,11 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request)
-          .then((response) => {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-            return response;
-          })
-          .catch(() => cached)
-      );
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
